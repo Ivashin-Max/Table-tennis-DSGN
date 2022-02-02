3950f8d7ebb4400d568ab53e19fa84200673a57d
Ivashin-Max <ivashinmax@gmail.com>
2022-02-02 16:25:23 +0300

    vk-subscribe delete

--- src/actions/initApp.jsx
+++ src/actions/initApp.jsx
@@ -1,30 +1,29 @@
 import { setSpreadsheets } from '../store/reducer';
-import { vkSubscribe } from './vk';
 import { getDivisionsSpreadsheets } from './renderNav';
 import url from '../static/url.json';
 
 export const initApp = () => async (dispatch, getState) => {
-	//Получаем ссылки из настроечной таблицы
+  //Получаем ссылки из настроечной таблицы
   const test = await getDivisionsSpreadsheets(url.TEST_settingsURL)
-  .catch((e)=>{
-    console.log('Ошибка доступа к таблице', e);
-    alert(`Ошибка доступа к таблице \n ${e}`)});
+    .catch((e) => {
+      console.log('Ошибка доступа к таблице', e);
+      alert(`Ошибка доступа к таблице \n ${e}`)
+    });
 
-	//Кладём их в стор
+  //Кладём их в стор
   dispatch(setSpreadsheets({
-		free: {url:test.free.url, tournaments: test.free.tournaments},
-		first: {url:test.first.url, tournaments: test.first.tournaments},
-    second: {url:test.second.url, tournaments: test.second.tournaments},
-    third:{url:test.third.url, tournaments: test.third.tournaments},
-    high: {url:test.high.url, tournaments: test.high.tournaments},
-    ttClub: {url:test.ttClub.url, tournaments: test.ttClub.tournaments},
+    free: { url: test.free.url, tournaments: test.free.tournaments },
+    first: { url: test.first.url, tournaments: test.first.tournaments },
+    second: { url: test.second.url, tournaments: test.second.tournaments },
+    third: { url: test.third.url, tournaments: test.third.tournaments },
+    high: { url: test.high.url, tournaments: test.high.tournaments },
+    ttClub: { url: test.ttClub.url, tournaments: test.ttClub.tournaments },
     links: test.links
   }))
 
-	const table = getState();
-	console.groupCollapsed('Сходили в настроечную таблицу, теперь в сторе хранятся следующие данные по турнирам');
-	console.log(table.test);
-	console.groupEnd();
-  vkSubscribe();
+  const table = getState();
+  console.groupCollapsed('Сходили в настроечную таблицу, теперь в сторе хранятся следующие данные по турнирам');
+  console.log(table.test);
+  console.groupEnd();
 }
 
