70ca662b9289226933078f5d10e009c7654aa78e
Ivashin-Max <ivashinmax@gmail.com>
2022-04-05 09:27:39 +0300

    testURL

--- src/actions/initApp.jsx
+++ src/actions/initApp.jsx
@@ -4,27 +4,28 @@ import { getDivisionsSpreadsheets } from './renderNav';
 import url from '../static/url.json';
 
 export const initApp = () => async (dispatch, getState) => {
-	//Получаем ссылки из настроечной таблицы
-  const test = await getDivisionsSpreadsheets(url.settingsURL)
-  .catch((e)=>{
-    console.log('Ошибка доступа к таблице', e);
-    alert(`Ошибка доступа к таблице \n ${e}`)});
+  //Получаем ссылки из настроечной таблицы
+  const test = await getDivisionsSpreadsheets(url.TEST_settingsURL)
+    .catch((e) => {
+      console.log('Ошибка доступа к таблице', e);
+      alert(`Ошибка доступа к таблице \n ${e}`)
+    });
 
-	//Кладём их в стор
+  //Кладём их в стор
   dispatch(setSpreadsheets({
-		free: {url:test.free.url, tournaments: test.free.tournaments},
-		first: {url:test.first.url, tournaments: test.first.tournaments},
-    second: {url:test.second.url, tournaments: test.second.tournaments},
-    third:{url:test.third.url, tournaments: test.third.tournaments},
-    high: {url:test.high.url, tournaments: test.high.tournaments},
-    ttClub: {url:test.ttClub.url, tournaments: test.ttClub.tournaments},
+    free: { url: test.free.url, tournaments: test.free.tournaments },
+    first: { url: test.first.url, tournaments: test.first.tournaments },
+    second: { url: test.second.url, tournaments: test.second.tournaments },
+    third: { url: test.third.url, tournaments: test.third.tournaments },
+    high: { url: test.high.url, tournaments: test.high.tournaments },
+    ttClub: { url: test.ttClub.url, tournaments: test.ttClub.tournaments },
     links: test.links
   }))
 
-	const table = getState();
-	console.groupCollapsed('Сходили в настроечную таблицу, теперь в сторе хранятся следующие данные по турнирам');
-	console.log(table.test);
-	console.groupEnd();
+  const table = getState();
+  console.groupCollapsed('Сходили в настроечную таблицу, теперь в сторе хранятся следующие данные по турнирам');
+  console.log(table.test);
+  console.groupEnd();
   vkSubscribe();
 }
 
